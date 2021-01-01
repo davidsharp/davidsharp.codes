4a790e94861d39a272144202c5691aa3f0ddf8dc
davidsharp <hello@davidsharp.codes>
2021-01-01 17:27:24 +0000

    Add optional box header

--- src/components/box.js
+++ src/components/box.js
@@ -1,13 +1,25 @@
 import React from 'react'
 
-const Box = ({children}) => <section style={{
+/*
+  header in the box, or above the box?
+*/
+
+const Box = ({header,children}) => <>
+<section style={{
   backgroundColor:'white',
   boxShadow: '5px 5px #444',
   border:'5px solid #333',
   padding: '.8em',
   marginBottom: '8px'
   }}>
+  {header && <BoxHeader>{header}</BoxHeader>}
   {children}
 </section>
+</>
+
+const BoxHeader = ({children}) => <h2 style={{
+  marginBottom: '.8em',
+  borderBottom: 'solid'
+}}>{children}</h2>
 
 export default Box
