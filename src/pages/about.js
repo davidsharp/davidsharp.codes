88cf17cc5bcee01da10215163e8e591358669370
davidsharp <hello@davidsharp.codes>
2021-01-01 18:13:17 +0000

    Playing around with generated about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,16 +8,19 @@ import Bio from '../components/bio'
 import Box from '../components/box'
 import Colour from '../components/colour'
 
+import data from '../../content/about-site.json'
+
+import {startCase} from 'lodash'
+
 const SecondPage = () => (
   <Layout>
     <SEO title="About" />
     <Bio header/>
     <Box header="About this site">
-      <p>This site was built using Gatsby and running on Netlify</p>
-      <p>The typeface is Helvetica</p>
-      <p>The palette uses <Colour hex="FEDF00"/> for the background</p>
+      <p>{`This site was built using ${data['built using']} and running on ${data['running on']}`}</p>
+      <p>{`The typeface is ${data.typeface}`}</p>
+      <p>{`The palette uses `}<Colour hex={data.background.toUpperCase()}/>{` for the background`}</p>
     </Box>
-    <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
 
