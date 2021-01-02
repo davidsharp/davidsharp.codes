36a5df090235f98d412331e7383c2fbfcc404b68
davidsharp <hello@davidsharp.codes>
2021-01-02 13:24:13 +0000

    Adds Gravatar to bio

--- src/components/bio.js
+++ src/components/bio.js
@@ -7,6 +7,16 @@ const Bio = ({header=false}) => <Box header={header?"About David":null}>
   <p><strong>David Sharp</strong> <em>(he/him)</em> is a "code wizard" living in Brighton</p>
   <p>David unconditionally loves JavaScript, and can often be found <a href="https://charanga.com">writing apps in exchange for money</a></p>
   <p>David also pretends to be interested in design, learns Japanese 'for the culture', promises he'll one day make a video game and owns a guitar (and at least one pedal)</p>
-  </Box>
+  <div style={{width:'100%'}}><img
+    style={{
+      marginLeft:'calc(50% - 100px)',
+      marginRight:'calc(50% - 100px)',
+      marginTop:'.3em',
+      marginBottom:0,
+      border:'solid #333'
+    }}
+    src="https://s.gravatar.com/avatar/78b4ada96c0948d94c8d6cd573736087?s=200"
+  /></div>
+</Box>
 
 export default Bio
