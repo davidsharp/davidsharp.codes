8bb2e63ec493dfe7787244b2594c543bc9591321
davidsharp <hello@davidsharp.codes>
2021-01-02 14:01:38 +0000

    Flexbox-ify the bio

--- src/components/bio.js
+++ src/components/bio.js
@@ -3,20 +3,34 @@ import Box from './box'
 
 import './bio.css'
 
-const Bio = ({header=false}) => <Box header={header?"About David":null}>
+const Bio = ({header=false}) => <Box
+header={header?"About David":null}
+style={{
+  display:'flex',
+  flexFlow:'row wrap',
+  justifyContent:'space-around',
+  alignItems: 'center'
+}}>
+  <div style={{
+    minWidth:'300px',
+    flex:'3 1 0'
+  }}>
   <p><strong>David Sharp</strong> <em>(he/him)</em> is a "code wizard" living in Brighton</p>
   <p>David unconditionally loves JavaScript, and can often be found <a href="https://charanga.com">writing apps in exchange for money</a></p>
   <p>David also pretends to be interested in design, learns Japanese 'for the culture', promises he'll one day make a video game and owns a guitar (and at least one pedal)</p>
-  <div style={{width:'100%'}}><img
+  </div>
+  <img
     style={{
-      marginLeft:'calc(50% - 100px)',
-      marginRight:'calc(50% - 100px)',
-      marginTop:'.3em',
-      marginBottom:0,
-      border:'solid #333'
+      maxWidth:200,
+      maxHeight:200,
+      border:'solid #333',
+      margin: '.8em',
+      flex: '1 0 0',
+      height: 200
     }}
     src="https://s.gravatar.com/avatar/78b4ada96c0948d94c8d6cd573736087?s=200"
-  /></div>
+    alt="David in Kyoto, December 2016"
+  />
 </Box>
 
 export default Bio
