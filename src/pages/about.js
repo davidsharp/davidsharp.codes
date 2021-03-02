5906853b6fd90b350fe7a68b13ae5d711c5ec4e4
davidsharp <hello@davidsharp.codes>
2021-03-02 20:20:33 +0000

    Date takes a month _index_
    0-indexed months, yuk

--- src/pages/about.js
+++ src/pages/about.js
@@ -27,7 +27,7 @@ const AboutPage = () => (
     <Box header="Experience">
       <p>{/*<strong>2014</strong> - */}Received a 2:1 BSc(Hons) Software Engineering from University of Brighton in 2014; mostly Java-focussed, but completed modules that touched on databases, embedded systems, computer graphics and such</p>
       <p>{/*<strong>2015-present</strong> - */}{`Currently working at `}<A href="https://charanga.com">Charanga</A>{`, started in 2015 (>`}{
-        differenceInYears(new Date(),new Date(2015,2,23))
+        differenceInYears(Date.now(),new Date(2015,1,23))
       }{` years), building Android, iOS and Desktop apps primarily in JavaScript. Initially using Cordova and AngularJS, but these days using Electron/React and React Native`}</p>
     </Box>
     <Kofi name={'davidsharp'} text={'Tip me!'} />
