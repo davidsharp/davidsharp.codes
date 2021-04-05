f78fe1d90735cff0cb976993edc16b1afbde06c4
davidsharp <hello@davidsharp.codes>
2021-04-05 14:09:07 +0100

    Added a little "Personal Projects" section to About

--- src/pages/about.js
+++ src/pages/about.js
@@ -24,6 +24,14 @@ const AboutPage = () => (
       <p>{`The typeface is ${data.typeface}`}</p>
       <p>{`The palette uses `}<Colour hex={data.background.toUpperCase()}/>{` for the background`}</p>
     </Box>
+    <Box header="Personal Projects">
+      <p>A lot of small toy projects can be found on my <A href="https://github.com/davidsharp">GitHub</A> (and a number of small plugins and scripts can be found on my <A href="https://gists.github.com/davidsharp">Gists</A>), mostly these exist to play around with some tech or solve a very small problem for myself. A couple of bigger things that I'm proud of though:</p>
+      <ul>
+        <li><A href="https://twitter.com/yearinfractions">{`Year in Fractions`}</A>{` Twitter bot inspired by @year_progress, powered by Express, tweets daily how far through the year we are as a (usually approximate) fraction`}</li>
+        <li><A href="https://puppetdf.glitch.me">{`PuppetDF`}</A>{`, a hacky proof-of-concept that's turned out to be surprisingly useful. An app that takes a URL and uses Puppeteer (a headless Chromium browser) to output a PDF`}</li>
+        <li><A href="https://slice-or-substr.glitch.me">{`slice-or-substr`}</A>{`, a kind of broken toy that demonstrates how slice, substring and substr all work in JavaScript, because I can never remember`}</li>
+      </ul>
+    </Box>
     <Box header="Experience">
       <p>{/*<strong>2014</strong> - */}Received a 2:1 BSc(Hons) Software Engineering from University of Brighton in 2014; mostly Java-focussed, but completed modules that touched on databases, embedded systems, computer graphics and such</p>
       <p>{/*<strong>2015-present</strong> - */}{`Currently working at `}<A href="https://charanga.com">Charanga</A>{`, started in 2015 (>`}{
