06f056cd43fbd8f91ac8ccf87638b4ad73fb71f3
davidsharp <hello@davidsharp.codes>
2021-10-12 15:04:58 +0100

    Fixed gists link

--- src/pages/about.js
+++ src/pages/about.js
@@ -25,7 +25,7 @@ const AboutPage = () => (
       <p>{`The palette uses `}<Colour hex={data.background.toUpperCase()}/>{` for the background`}</p>
     </Box>
     <Box header="Personal Projects">
-      <p>A lot of small toy projects can be found on my <A href="https://github.com/davidsharp">GitHub</A> (and a number of small plugins and scripts can be found on my <A href="https://gists.github.com/davidsharp">Gists</A>), mostly these exist to play around with some tech or solve a very small problem for myself. A couple of bigger things that I'm proud of though:</p>
+      <p>A lot of small toy projects can be found on my <A href="https://github.com/davidsharp">GitHub</A> (and a number of small plugins and scripts can be found on my <A href="https://gist.github.com/davidsharp">Gists</A>), mostly these exist to play around with some tech or solve a very small problem for myself. A couple of bigger things that I'm proud of though:</p>
       <ul>
         <li><A href="https://twitter.com/yearinfractions">{`Year in Fractions`}</A>{` Twitter bot inspired by @year_progress, powered by Express, tweets daily how far through the year we are as a (usually approximate) fraction`}</li>
         <li><A href="https://puppetdf.glitch.me">{`PuppetDF`}</A>{`, a hacky proof-of-concept that's turned out to be surprisingly useful. An app that takes a URL and uses Puppeteer (a headless Chromium browser) to output a PDF`}</li>
