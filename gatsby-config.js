c8fc89526f8844f43159ff2f1d35b405201b3ecf
davidsharp <hello@davidsharp.codes>
2022-01-17 11:44:15 +0000

    Update theme colour to match site colour

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,8 +21,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#fedf00`,
+        theme_color: `#fedf00`,
         display: `minimal-ui`,
         icon: `src/images/icon.svg`, // This path is relative to the root of the site.
       },
