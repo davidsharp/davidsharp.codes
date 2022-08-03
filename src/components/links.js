f6df2722e6e44ec8b24190e06b6b9604b2a87bb6
davidsharp <hello@davidsharp.codes>
2022-08-03 12:23:24 +0100

    Hide navbar link separators from screen readers

--- src/components/links.js
+++ src/components/links.js
@@ -4,11 +4,11 @@ import "./icon.css"
 
 const Links = props => <div className="links" style={{textAlign:'center'}}>
   {props.components ? props.components.map((Link,i)=><>
-      {i!=0 && `・`}
+      {i!=0 && <span aria-hidden="true">{`・`}</span>}
       <Link/>
     </>
   ):React.Children.map(props.children,(child,i)=><>
-    {i!=0 && `・`}
+    {i!=0 && <span aria-hidden="true">{`・`}</span>}
     {child}
   </>)}
 </div>
