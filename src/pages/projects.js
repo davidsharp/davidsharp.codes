3d3ae34444eef4c43c1393a21847040ba0eb542f
David Sharp <hello@davidsharp.codes>
2022-10-13 13:34:18 +0100

    Bring the games up above the bitbar plugins

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -12,7 +12,7 @@ const ProjectsPage = () => (
     <SEO title="Projects" />
     {/* todo - add actual projects */}
     <Box header="Personal Projects">
-      <p>A lot of small toy projects can be found on my <A href="https://github.com/davidsharp">GitHub</A> and <A href="https://glitch.com/@davidsharp">Glitch</A> (and a number of small plugins and scripts can be found on my <A href="https://gists.github.com/davidsharp">Gists</A>), mostly these exist to play around with some tech or solve a very small problem for myself. A couple of bigger things that I'm proud of though:</p>
+      <p>A lot of small toy projects can be found on my <A href="https://github.com/davidsharp">GitHub</A> and <A href="https://glitch.com/@davidsharp">Glitch</A> (and a number of small plugins and scripts can be found on my <A href="https://gists.github.com/davidsharp">Gists</A>), mostly these exist to play around with some tech or solve a very small problem for myself. Here's an assortment of things that I'm proud of though:</p>
       <ul>
         <li><Tw href="https://twitter.com/yearinfractions">{`Year in Fractions`}</Tw>{`, a Twitter bot inspired by `}<Tw href="https://twitter.com/year_progress">{`@year_progress`}</Tw>{`, powered by Express, tweets daily how far through the year we are as a (usually approximate) fraction`}</li>
         <li><A href="https://puppetdf.glitch.me">{`PuppetDF`}</A>{`, what started as a hacky proof-of-concept that turned out to be surprisingly useful. An app that takes a URL and uses Puppeteer (a headless Chromium browser) to output a PDF`}</li>
@@ -21,7 +21,15 @@ const ProjectsPage = () => (
         <li><GH href="https://github.com/davidsharp/davidsharp.codes">{`This very site`}</GH>{`, a Gatsby site running on Netlify`}</li>
         <li><A href="https://glitch.com/~christmas-eve-eve">{`Christmas Eve Eve`}</A>{`, a very dumb single-page thing based on an old XKCD`}</li>
       </ul>
-      <hr/>
+    </Box>
+    <Box header="Games">
+      <p>I've casually toyed with making small games here and there, and here's what has stuck:</p>
+      <ul>
+        <li><Itch href="https://davidsharp.itch.io/puff-puff-pass-british-bulldog">British Bulldog</Itch>, a game made in collaboration with a few friends with minimal communication, built in the Lua-based framework Love2D, most of my work was refactoring, completing features and polishing, but I also added a splash screen inspired by old cracktros</li>
+        <li><Itch href="https://davidsharp.itch.io/build-jam">Build-n-Blocks</Itch>, a game made as part of a small "Build" themed game jam in a fortnight, also built in Love2D. The game is a 1-bit falling block puzzle game, with a Gameboy RPG-esque overworld</li>
+      </ul>
+    </Box>
+    <Box header="Other coding miscellany">
       <p>Here's some <GH href="https://github.com/matryer/xbar">bitbar/xbar</GH> plugins for macOS (all GitHub gists):</p>
       <ul>
         <li><A href="https://gist.github.com/davidsharp/b57d34dba2124bc8d440a60699b82a4a">{`Weather`}</A>{`, a weather plugin that uses `}<A href="https://wttr.in">{`wttr.in`}</A></li>
@@ -30,13 +38,6 @@ const ProjectsPage = () => (
         <li><A href="https://gist.github.com/davidsharp/e0bfa84a657ef113b3d59f24767b8314">{`COVID Stats`}</A>{`, a plugin that uses `}<A href="https://diseases.sh">{`diseases.sh`}</A>{` to get the country's COVID stats`}</li>
         <li><A href="https://gist.github.com/davidsharp/209485fbb7c4d6cbbb362c6139e38f66">{`Toggle Mono/Stereo audio`}</A>{` (may no longer work), a plugin that adds a button to toggle accessibility options for mono/stereo audio`}</li>
         <li><A href="https://gist.github.com/davidsharp/164c69bbe60925122c727f526eaeab89">{`Clipboard Spy`}</A>{`, a plugin that shows what's on your clipboard, with an option to clear it`}</li>
-      </ul>
-    </Box>
-    <Box header="Games">
-      <p>I've casually toyed with making small games here and there, and here's what has stuck:</p>
-      <ul>
-        <li><Itch href="https://davidsharp.itch.io/puff-puff-pass-british-bulldog">British Bulldog</Itch>, a game made in collaboration with a few friends with minimal communication, built in the Lua-based framework Love2D, most of my work was refactoring, completing features and polishing, but I also added a splash screen inspired by old cracktros</li>
-        <li><Itch href="https://davidsharp.itch.io/build-jam">Build-n-Blocks</Itch>, a game made as part of a small "Build" themed game jam in a fortnight, also built in Love2D. The game is a 1-bit falling block puzzle game, with a Gameboy RPG-esque overworld</li>
       </ul>
     </Box>
   </Layout>
