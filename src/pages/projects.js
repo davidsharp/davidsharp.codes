a30ae9dbec3ab8448ebfcce7dbe353d0ab18d4f3
David Sharp <hello@davidsharp.codes>
2022-10-21 00:08:11 +0100

    Make consnake its own component and run it in projects too

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -7,9 +7,13 @@ import Box from '../components/box'
 import A from '../components/anchor'
 import {GH, Tw, Gl, Itch} from '../components/social-link'
 
+import Snake from '../components/snake'
+
 const ProjectsPage = () => (
   <Layout>
     <SEO title="Projects" />
+    {/* todo - don't just dump Snake here*/}
+    <Snake/>
     {/* todo - section picker*/}
     <Box header="Personal Projects">
       <p>A lot of small toy projects can be found on my <A href="https://github.com/davidsharp">GitHub</A> and <A href="https://glitch.com/@davidsharp">Glitch</A> (and a number of small plugins and scripts can be found on my <A href="https://gists.github.com/davidsharp">Gists</A>), mostly these exist to play around with some tech or solve a very small problem for myself. Here's an assortment of things that I'm proud of though:</p>
@@ -17,7 +21,7 @@ const ProjectsPage = () => (
         <li><Tw href="https://twitter.com/yearinfractions">{`Year in Fractions`}</Tw>{`, a Twitter bot inspired by `}<Tw href="https://twitter.com/year_progress">{`@year_progress`}</Tw>{`, powered by Express, tweets daily how far through the year we are as a (usually approximate) fraction`}</li>
         <li><GH href="https://github.com/davidsharp/davidsharp.codes">{`This very site`}</GH>{`, a Gatsby site running on Netlify`}</li>
         <li><GH href="https://github.com/davidsharp/wizzbo">{`Wizzbo`}</GH>{`, a Discord bot that acts as a playground for me to try out ideas, but with the functionality to deploy subsets of commands as their own sub-bots`}</li>
-        <li><GH href="https://github.com/davidsharp/consnake">{`Consnake`}</GH>{`, Snake, but playable in the devtools (playable from a `}<A href="/404">{`404`}</A>{` page)`}</li>
+        <li><GH href="https://github.com/davidsharp/consnake">{`Consnake`}</GH>{`, Snake, but playable in the devtools (opening devtools here will start the game 👀)`}</li>
         <li><Gl href="https://glitch.com/~christmas-eve-eve">{`Christmas Eve Eve`}</Gl>{`, a very dumb single-page thing based on an old XKCD`}</li>
         <li><A href="https://puppetdf.glitch.me">{`PuppetDF`}</A>{`, what started as a hacky proof-of-concept that turned out to be surprisingly useful. An app that takes a URL and uses Puppeteer (a headless Chromium browser) to output a PDF`}</li>
         <li><A href="https://slice-or-substr.glitch.me">{`slice-or-substr`}</A>{`, a kind of broken toy that demonstrates how slice, substring and substr all work in JavaScript, because I can never remember`}</li>
