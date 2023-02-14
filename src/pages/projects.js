902d1cdc33d11c1502b2ee57ee886307c1043e18
David Sharp <hello@davidsharp.codes>
2023-02-14 12:21:10 +0000

    grammar fix

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -33,7 +33,7 @@ const ProjectsPage = () => (
       <ul>
         <li><Itch href="https://davidsharp.itch.io/puff-puff-pass-british-bulldog">British Bulldog</Itch>, a game made in collaboration with a few friends with minimal communication, built in the Lua-based framework <A href="https://love2d.org/">LÖVE</A>, most of my work was refactoring, completing features and polishing, but I also added a splash screen inspired by old cracktros</li>
         <li><Itch href="https://davidsharp.itch.io/build-jam">Build-n-Blocks</Itch>, a game made as part of a small "Build" themed game jam in a fortnight, also built in <A href="https://love2d.org/">LÖVE</A>. The game is a 1-bit falling block puzzle game, with a Gameboy RPG-esque overworld</li>
-        <li><Itch href="https://davidsharp.itch.io/card-sharpshooter">Card Sharpshooter</Itch>, an Playing Card-themed FPS prototype built in Godot. A collaborative effort, "my" game of 3 we worked on together.</li>
+        <li><Itch href="https://davidsharp.itch.io/card-sharpshooter">Card Sharpshooter</Itch>, a Playing Card-themed FPS prototype built in Godot. A collaborative effort, "my" game of 3 we worked on together.</li>
       </ul>
       <p>I've also started entering a few small game jams, to experiment with prototypes and build the habit of finishing small projects:</p>
       <ul>
