bcf401a397c260063fd8bf2f0a80c69983ea38fc
David Sharp <hello@davidsharp.codes>
2023-03-19 19:19:30 +0000

    Re-adds the 404 page

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,20 +3,22 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import config from "./deno.json" assert { type: "json" };
-import * as $0 from "./routes/[name].tsx";
+import * as $0 from "./routes/_404.tsx";
 import * as $1 from "./routes/about.tsx";
 import * as $2 from "./routes/api/joke.ts";
 import * as $3 from "./routes/index.tsx";
 import * as $4 from "./routes/projects.tsx";
+import * as $5 from "./routes/x[name].tsx";
 import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
   routes: {
-    "./routes/[name].tsx": $0,
+    "./routes/_404.tsx": $0,
     "./routes/about.tsx": $1,
     "./routes/api/joke.ts": $2,
     "./routes/index.tsx": $3,
     "./routes/projects.tsx": $4,
+    "./routes/x[name].tsx": $5,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
---
+++ routes/_404.tsx
@@ -0,0 +1,23 @@
+import Layout from "../src/components/layout.jsx"
+import SEO from "../src/components/seo.jsx"
+import Box from '../src/components/box.jsx'
+//import Snake from '../src/components/snake'
+const Snake = null
+
+import { UnknownPageProps } from "$fresh/server.ts";
+
+
+
+const NotFoundPage = ({ url }: UnknownPageProps) => (
+  <Layout>
+    <SEO title="404: Not found" />
+    <Snake/>
+    <Box header="Not Found">
+      <p>{`The page you tried to navigate to (${url.pathname}) doesn't seem to exist 😱`}</p>
+    </Box>
+  </Layout>
+)
+
+export default NotFoundPage
+
+
