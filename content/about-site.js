8d84d2cde579ece555d944e3ea087b3131d1368c
David Sharp <hello@davidsharp.codes>
2023-03-27 00:25:56 +0100

    Commit to running on Deno Deploy

--- content/about-site.js
+++ content/about-site.js
@@ -2,6 +2,6 @@ export default {
   "background":"ffb6c1",
   "typeface":"Fredoka One in the headings and Balsamiq Sans everywhere else",
   "built using": "Fresh",
-  "running on": "Deno Deploy(?)",
+  "running on": "Deno Deploy",
   "--note": "above code be generated?"
 }
