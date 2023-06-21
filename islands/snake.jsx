9e66f3914352dfc612e6c0a9fb6fc6ba71dde21a
David Sharp <hello@davidsharp.codes>
2023-06-21 10:08:29 +0100

    Use emoji version of consnake

--- islands/snake.jsx
+++ islands/snake.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState, useRef } from "preact/hooks"
 
-import Game from 'https://cdn.jsdelivr.net/gh/davidsharp/consnake/snake.js'
+import Game from 'https://esm.sh/gh/davidsharp/consnake@1.0.1-emoji/snake.js'
 import devtools from 'https://esm.sh/v113/devtools-detect@4.0/index.js'
 
 export default function Snake(){
