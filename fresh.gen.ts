8f86cd8af40c0fb61e78c76ab84b81cdc59e78a4
David Sharp <hello@davidsharp.codes>
2023-07-05 10:58:40 +0100

    Update deno and fresh

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,7 +2,6 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/_404.tsx";
 import * as $1 from "./routes/about.tsx";
 import * as $2 from "./routes/api/joke.ts";
@@ -28,7 +27,6 @@ const manifest = {
     "./islands/snake.jsx": $$2,
   },
   baseUrl: import.meta.url,
-  config,
 };
 
 export default manifest;
