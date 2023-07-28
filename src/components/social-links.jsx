97c27d31dabad5e5712affb90052e42b043f2131
David Sharp <hello@davidsharp.codes>
2023-07-28 17:48:36 +0100

    Demote Twitter (consider removing)

--- src/components/social-links.jsx
+++ src/components/social-links.jsx
@@ -10,12 +10,12 @@ import {
 } from './icons.jsx'
 
 const SocialLinks = props => <Links>
-  <A title="Twitter" href="https://twitter.com/elitefreq" ariaHidden><Twitter/></A>
   <A title="Instagram" href="https://www.instagram.com/coin.op/" ariaHidden><Instagram/></A>
   <A title="GitHub" href="https://github.com/davidsharp" ariaHidden><Github/></A>
   <A title="Glitch" href="https://glitch.com/@davidsharp" ariaHidden><Glitch/></A>
   <A title="LinkedIn" href="https://www.linkedin.com/in/david-sharp-9b054361/" ariaHidden><LinkedIn/></A>
   <A title="Itch.io" href="https://davidsharp.itch.io/" ariaHidden><ItchIo/></A>
+  <A title="Twitter" href="https://twitter.com/elitefreq" ariaHidden><Twitter style={{background: 'linear-gradient(45deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0) 40%, rgba(255,0,0,1) 40%,rgba(255,0,0,1) 60%, rgba(0,0,0,0) 60%, rgba(0,0,0,0) 100%)'}}/></A>
   {/*TODO - add cohost link*/}
 </Links>
 
