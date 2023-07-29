d00312874d4b0cf4dd6a40aa6ddc96d55d8f6e1e
David Sharp <hello@davidsharp.codes>
2023-07-29 12:48:20 +0100

    Looking at re-working how icons are imported

--- src/components/icons.jsx
+++ src/components/icons.jsx
@@ -2,11 +2,25 @@ export {
   AiFillHome as Home
 } from 'react-icons/ai';
 
-export {
-  SiTwitter as Twitter,
+import {
+  siTwitter
+} from 'https://esm.sh/simple-icons@9.7.0'
+
+import {
   SiInstagram as Instagram,
   SiGithub as Github,
   SiGlitch as Glitch,
   SiLinkedin as LinkedIn,
   SiItchdotio as ItchIo,
-} from 'react-icons/si'
+} from 'react-icons/si';
+
+const Twitter = () => <svg role="img" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d={siTwitter.path}/></svg>
+
+export {
+  Twitter,
+  Instagram,
+  Github,
+  Glitch,
+  LinkedIn,
+  ItchIo,
+}
--- src/components/social-links.jsx
+++ src/components/social-links.jsx
@@ -15,7 +15,7 @@ const SocialLinks = props => <Links>
   <A title="Glitch" href="https://glitch.com/@davidsharp" ariaHidden><Glitch/></A>
   <A title="LinkedIn" href="https://www.linkedin.com/in/david-sharp-9b054361/" ariaHidden><LinkedIn/></A>
   <A title="Itch.io" href="https://davidsharp.itch.io/" ariaHidden><ItchIo/></A>
-  {/*<A title="Twitter" href="https://twitter.com/elitefreq" ariaHidden><Twitter style={{background: 'linear-gradient(45deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0) 40%, rgba(255,0,0,1) 40%,rgba(255,0,0,1) 60%, rgba(0,0,0,0) 60%, rgba(0,0,0,0) 100%)'}}/></A>*/}
+  <A title="Twitter" href="https://twitter.com/elitefreq" ariaHidden><Twitter style={{background: 'linear-gradient(45deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0) 40%, rgba(255,0,0,1) 40%,rgba(255,0,0,1) 60%, rgba(0,0,0,0) 60%, rgba(0,0,0,0) 100%)'}}/></A>
   {/*TODO - add cohost link*/}
 </Links>
 
