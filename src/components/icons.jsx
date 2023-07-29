b96b708475e702eb8db2971a5b788e3ded392b15
David Sharp <hello@davidsharp.codes>
2023-07-29 22:22:05 +0100

    Replace simple icons imports

--- src/components/icons.jsx
+++ src/components/icons.jsx
@@ -3,18 +3,22 @@ export {
 } from 'react-icons/ai';
 
 import {
-  siTwitter
+  siTwitter,
+  siInstagram,
+  siGithub,
+  siGlitch,
+  siLinkedin,
+  siItchdotio,
 } from 'https://esm.sh/simple-icons@9.7.0'
 
-import {
-  SiInstagram as Instagram,
-  SiGithub as Github,
-  SiGlitch as Glitch,
-  SiLinkedin as LinkedIn,
-  SiItchdotio as ItchIo,
-} from 'react-icons/si';
+const Icon = ({path}) => <svg stroke="currentColor" fill="currentColor" stroke-width="0"  role="img" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d={path}/></svg>
 
-const Twitter = () => <svg role="img" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d={siTwitter.path}/></svg>
+const Twitter = () => <Icon path={siTwitter.path}/>
+const Instagram = () => <Icon path={siInstagram.path}/>
+const Github = () => <Icon path={siGithub.path}/>
+const Glitch = () => <Icon path={siGlitch.path}/>
+const LinkedIn = () => <Icon path={siLinkedin.path}/>
+const ItchIo = () => <Icon path={siItchdotio.path}/>
 
 export {
   Twitter,
