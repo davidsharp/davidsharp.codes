eaf1c5a57e3fa97da5e78f8bb7eb8c7a07fa0f4d
David Sharp <hello@davidsharp.codes>
2024-04-23 18:45:25 +0100

    Tweaked bitbar section

--- routes/projects/index.tsx
+++ routes/projects/index.tsx
@@ -53,14 +53,14 @@ const ProjectsPage = ({route}) => (
     <Box header="Other coding miscellany">
       <p>I've participated in <A href="https://adventofcode.com">Advent of Code</A> for a number of years (but have yet to <em>fully</em> complete a year), usually in terse JavaScript, and keep my code here: <GH href="https://github.com/davidsharp/advent-of-code">davidsharp/advent-of-code</GH></p>
       <hr/>
-      <p>Here's some <GH href="https://github.com/matryer/xbar">bitbar/xbar</GH> plugins for macOS (all GitHub gists):</p>
+      <p>Here's some <GH href="https://github.com/matryer/xbar">xbar (fka bitbar)</GH> plugins for macOS (all GitHub gists):</p>
       <ul>
         <li><A href="https://gist.github.com/davidsharp/b57d34dba2124bc8d440a60699b82a4a">{`Weather`}</A>{`, a weather plugin that uses `}<A href="https://wttr.in">{`wttr.in`}</A></li>
         <li><A href="https://gist.github.com/davidsharp/767f69c090c7381af70ff50325f56bca">{`Spotify`}</A>{`, a plugin that displays the currently playing song, with some shortcuts and controls`}</li>
         <li><A href="https://gist.github.com/davidsharp/707417fdc85b3128f91bb075fb17fbcc">{`Shuffle Spotify`}</A>{`, a plugin that adds a shuffle/unshuffle button to the menu bar`}</li>
+        <li><A href="https://gist.github.com/davidsharp/164c69bbe60925122c727f526eaeab89">{`Clipboard Spy`}</A>{`, a plugin that shows what's on your clipboard, with an option to clear it`}</li>
         <li><span style={{textDecoration:'line-through'}}><A href="https://gist.github.com/davidsharp/e0bfa84a657ef113b3d59f24767b8314">{`COVID Stats`}</A>{`, a plugin that uses `}<A href="https://diseases.sh">{`diseases.sh`}</A>{` to get the country's COVID stats`}</span> <span class="small">(API no longer updated)</span></li>
         <li><A href="https://gist.github.com/davidsharp/209485fbb7c4d6cbbb362c6139e38f66">{`Toggle Mono/Stereo audio`}</A>{` (may no longer work), a plugin that adds a button to toggle accessibility options for mono/stereo audio`}</li>
-        <li><A href="https://gist.github.com/davidsharp/164c69bbe60925122c727f526eaeab89">{`Clipboard Spy`}</A>{`, a plugin that shows what's on your clipboard, with an option to clear it`}</li>
       </ul>
     </Box>
   </Layout>
