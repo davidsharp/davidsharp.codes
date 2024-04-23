2abc03b3ad462ccfaabaa37686901c0d7c51e3b6
David Sharp <hello@davidsharp.codes>
2024-04-23 18:03:06 +0100

    Adds Ball Room to my jam game list

--- routes/projects/index.tsx
+++ routes/projects/index.tsx
@@ -36,8 +36,9 @@ const ProjectsPage = ({route}) => (
       </ul>
       <p>I've also started entering a few small game jams, to experiment with prototypes and build the habit of finishing small projects:</p>
       <ul>
-        <li><Itch href="https://davidsharp.itch.io/pongaconda">Pongaconda</Itch>, a rough prototype of a game combining Pong and Snake, built in a few hours for PongJam</li>
         <li><Itch href="https://davidsharp.itch.io/bunslinger">Bunslinger at High Noon</Itch>, an arcade-y restaurant sim game in the style of a Nokia 3310 for Nokia Jam 5</li>
+        <li><Itch href="https://davidsharp.itch.io/ball-room-trijam-262">Ball Room</Itch>, a simple high score game made for the 3-hour weekly game jam, Trijam</li>
+        <li><Itch href="https://davidsharp.itch.io/pongaconda">Pongaconda</Itch>, a rough prototype of a game combining Pong and Snake, built in a few hours for PongJam</li>
       </ul>
     </Box>
     <Box header="Work in progress">
