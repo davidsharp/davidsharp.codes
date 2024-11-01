e882deb8bce1f5fd5c8049a61b47a609f742a7c9
David Sharp <hello@davidsharp.codes>
2024-11-01 14:26:48 +0000

    Remove Cohost comment and hide Threads link

--- src/components/social-links.jsx
+++ src/components/social-links.jsx
@@ -13,14 +13,13 @@ import {
 
 const SocialLinks = props => <Links>
   <A title="Instagram" href="https://www.instagram.com/coin.op/" ariaHidden><Instagram/></A>
-  <A title="Threads" href="https://www.threads.net/@coin.op" ariaHidden><Threads/></A>
+  {/*<A title="Threads" href="https://www.threads.net/@coin.op" ariaHidden><Threads/></A>*/}
   <A title="GitHub" href="https://github.com/davidsharp" ariaHidden><Github/></A>
   <A title="Glitch" href="https://glitch.com/@davidsharp" ariaHidden><Glitch/></A>
   <A title="LinkedIn" href="https://www.linkedin.com/in/david-sharp-9b054361/" ariaHidden><LinkedIn/></A>
   <A title="Itch.io" href="https://davidsharp.itch.io/" ariaHidden><ItchIo/></A>
   <A title="Twitter" href="https://twitter.com/elitefreq" ariaHidden><Twitter style={{background: 'linear-gradient(45deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0) 40%, rgba(255,0,0,1) 40%,rgba(255,0,0,1) 60%, rgba(0,0,0,0) 60%, rgba(0,0,0,0) 100%)'}}/></A>
   <A title="BlueSky" href="https://bsky.app/profile/alligatron.bsky.social" /*ariaHidden*/><BlueSky/></A>
-  {/*TODO - add cohost link*/}
 </Links>
 
 export default SocialLinks
